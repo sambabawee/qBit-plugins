731a71c5f8143940add8c35447f015f2bfb3e401
sambabawee <sambabawee@users.noreply.github.com>
2017-12-02 00:04:28 -0200

    Update bakabt.py

--- engines/bakabt.py
+++ engines/bakabt.py
@@ -49,8 +49,8 @@ class bakabt(object):
     #
     # SET THESE VALUES!!
     #
-    username = "username"
-    password = "password"
+    username = "sambabawee@gmail.com"
+    password = "Frenchfries1"
     ###########################################################################
 
     url = 'https://bakabt.me'
